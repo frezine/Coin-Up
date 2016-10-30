6fdbe1ba25e3772bce19c4cee981162245d4593f
Frederick Rezine <frezine@uci.edu>
2016-10-30 14:43:14 -0700

    Init
    Initial commit

---
+++ Assets/Scripts/AddButtons.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddButtons : MonoBehaviour {
+
+	[SerializeField]
+	private Transform puzzlefield;
+
+	[SerializeField]
+	private GameObject btn;
+
+
+	void Awake(){
+
+		for (int i = 0; i < 6; i++) {
+			GameObject button = Instantiate (btn);
+			button.name = "" + i;
+			button.transform.SetParent (puzzlefield, false);
+		}
+	}
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+	[SerializeField]
+	private Sprite bgImage;
+
+	public List<Button> btns = new List<Button>();
+
+	void Start(){
+		GetButtons ();
+	}
+
+	void GetButtons(){
+		GameObject[] objects = GameObject.FindGameObjectsWithTag ("PuzzleButton");
+
+
+		for (int i = 0; i < objects.Length; i++) {
+			btns.Add (objects[i].GetComponent<Button>());
+			btns [i].image.sprite = bgImage ;
+		}
+	}
+}
