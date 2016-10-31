416c5d1b23c6cc535b300dfaaddf50db8e6e56a8
Frederick Rezine <frezine@uci.edu>
2016-10-30 17:52:49 -0700

    Added Coin Values/Player Turns

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -14,7 +14,12 @@ public class GameController : MonoBehaviour {
 	[SerializeField]
 	private Sprite quarter;
 
+	private float p1Score = 0;
+	private float p2Score = 0;
+	private int turn = 0;
 
+	private int left = 0;
+	private int right;
 
 	public List<Button> btns = new List<Button>();
 
@@ -29,18 +34,21 @@ public class GameController : MonoBehaviour {
 
 
 		for (int i = 0; i < objects.Length; i++) {
+			float randomCoin = Random.Range (0, 4);
+			Debug.Log (randomCoin);
 			btns.Add (objects[i].GetComponent<Button>());
-			if (i % 4 == 0) {
+			if (randomCoin % 4 == 0) {
 				btns [i].image.sprite = penny;
-			} else if (i % 4 == 1) {
+			} else if (randomCoin % 4 == 1) {
 				btns [i].image.sprite = nickel ;
-			} else if (i % 4 == 2) {
+			} else if (randomCoin % 4 == 2) {
 				btns [i].image.sprite = dime ;
-			} else if (i % 4 == 3) {
+			} else if (randomCoin % 4 == 3) {
 				btns [i].image.sprite = quarter ;
 			}
 
 		}
+		right = objects.Length - 1;
 	}
 
 	void AddListeners(){
@@ -52,5 +60,59 @@ public class GameController : MonoBehaviour {
 	public void PickAPuzzle(){
 		string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
 		Debug.Log ("clicking button named " + name);
+
+		int coinIndex = int.Parse (UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+		Sprite currentCoin = btns [coinIndex].image.sprite;
+		float coinValue = 0f;
+		if (currentCoin == penny) {
+			coinValue = .01f;
+		} else if (currentCoin == nickel) {
+			coinValue = .05f;
+		} else if (currentCoin == dime) {
+			coinValue = .10f;
+		} else if (currentCoin == quarter) {
+			coinValue = .25f;
+		}
+
+		if (coinIndex == left) {
+			if (turn % 2 == 0) {
+				p1Score += coinValue;
+			} else {
+				p2Score += coinValue;
+			}
+
+			btns [coinIndex].interactable = false;
+			btns [coinIndex].image.color = new Color(0,0,0,0);
+			left++;
+			turn++;
+		} else if (coinIndex == right) {
+			if (turn % 2 == 0) {
+				p1Score += coinValue;
+			} else {
+				p2Score += coinValue;
+			}
+
+			btns [coinIndex].interactable = false;
+			btns [coinIndex].image.color = new Color(0,0,0,0);
+			right--;
+			turn++;
+		} else {
+			
+		}
+
+		StartCoroutine (PickedACoin ());
+
+		if (GameIsFinished ()) {
+			Debug.Log ("Player 1: " + p1Score + "; Player 2: " + p2Score);
+		}
+	}
+
+	IEnumerator PickedACoin(){
+		yield return new WaitForSeconds (1f);
+
+	 }
+
+	bool GameIsFinished(){
+		return left > right;
 	}
 }
